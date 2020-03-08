6d78eae27d38b1466240ee5d0a83312da20f3730
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2020-03-08 15:34:47 +0100

    VSCode ma problem

--- onp/main.c
+++ onp/main.c
@@ -1,7 +1,44 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
 
-int main(){
-    puts("Hello world!");
+float get_operation_result(float x1, float x2, char operation)
+{
+    switch (operation)
+    {
+        case '+':
+            return x1 + x2;
+
+        case '-':
+            return x1 - x2;
+
+        case '*':
+            return x1 * x2;
+
+        case '/':
+            if (x2 == 0)
+                puts("Can't divide by zero!");
+                return -1;
+            return x1 / x2;
+
+        default:
+            puts("Invalid operator!");
+            return -2;
+    }
+}
+
+float get_calculated(char onp[100][1000000])
+{
+    for(int i = 0; i < sizeof(onp)/sizeof(char); i++)
+    {
+
+
+    }
+
+}
+
+int main()
+{
+
     return 0;
 }
