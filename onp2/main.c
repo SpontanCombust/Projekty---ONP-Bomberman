7505babba82172e771b70429e5bbd76206f623aa
KMR3000 <61948369+KMR3000@users.noreply.github.com>
2020-03-30 06:50:28 +0200

    Add files via upload

---
+++ onp2/main.c
@@ -0,0 +1,17 @@
+#include "stack.h"
+#include "stack.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include "stack_handler.c"
+
+int main(){
+
+    char input[22]={'\0'};
+    puts("Enter Reverse Polish Notation:");
+    scanf("%21[^\n]s",input);
+    //Sprawdzenie czy wprowadzany ciag ma powyzej 20 znakow
+    if(strlen(input)>20){printf("Given input is too long!!!"); return -1;}
+    handle_element(input);
+    return 0;
+}
+
